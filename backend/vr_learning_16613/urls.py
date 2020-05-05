aa00030ae5b522e3107eb28c3e94f718d8215aaa
Crowdbotics <team@crowdbotics.com>
2020-05-05 03:37:58 +0000

    Update admin title

--- backend/vr_learning_16613/urls.py
+++ backend/vr_learning_16613/urls.py
@@ -32,9 +32,9 @@ urlpatterns = [
     path("rest-auth/registration/", include("rest_auth.registration.urls")),
 ]
 
-admin.site.site_header = "Vr Learning"
-admin.site.site_title = "Vr Learning Admin Portal"
-admin.site.index_title = "Vr Learning Admin"
+admin.site.site_header = "Vr Quarantine Hub"
+admin.site.site_title = "Vr Quarantine Hub Admin Portal"
+admin.site.index_title = "Vr Quarantine Hub Admin"
 
 # swagger
 schema_view = get_schema_view(
